e4bb2f02e35e99707f310c8905010f65bd614187
Vu Ha <64212632+havu95@users.noreply.github.com>
2021-09-29 12:42:32 +0200

    Delete test3.py

--- test3.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.training import checkpoint_utils as cp
-
-print(cp.load_variable('saved_parameters/FNL_MDBDP_2/BoundedFNLMDBDPd2nbNeur12nbHL2ndt12030Alpha100BSDE_1','NetWorkUZ1/enc_fc1/weights'))
-print(cp.load_variable('save/OneAssetMDBDPd2nbNeur12nbHL2ndt3010eta50BSDE_1','NetWorkUZ1/enc_fc1/weights'))
