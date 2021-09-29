6b942164f51bc567ac3e01f68fd67a05d9d19b68
Vu Ha <64212632+havu95@users.noreply.github.com>
2021-09-29 12:42:39 +0200

    Delete testtest.py

--- testtest.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.training import checkpoint_utils as cp
-
-
-print(cp.list_variables('save/MertonPWGd1nbNeur11nbHL2ndt30eta50BSDE_0'))
-print('-------------------------------------------------')
-#print(cp.load_variable('save/MertonPWGd1nbNeur11nbHL2ndt30eta50BSDE_1','NetWorkUZ1/enc_fc1/weights'))
-#print(cp.load_variable('saved_parameters/FNL_PWG_1/BoundedFNLPWGd1nbNeur11nbHL2ndt120Alpha100BSDE_1','NetWorkUZ1/enc_fc1/weights'))
